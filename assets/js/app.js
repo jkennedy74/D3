e79cae4bbd3679f6e037f284a8754f50c83d1e66
Juliana Kennedy <jkennedy@firstlook.biz>
2018-08-09 17:14:30 -0500

    initial data for further analysis

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,17 @@
-// D3 Scatterplot Assignment
+// // D3 Scatterplot Assignment
+
+// Using the D3 techniques we taught you in class, create a scatter plot that represents each state with circle elements. You'll code this graphic in the app.js file of your homework directory—make sure you pull in the data from data.csv by using the d3.csv function. Your scatter plot should ultimately appear like the image at the top of this section.
+
+
+// The x-values of the circles should match the demographic census data, while the y-values should represent the risk data.
+// Include state abbreviations in the circles.
+// Create and situate your axes and labels to the left and bottom of the chart.
+// Generate this chart in the d3.html file in your assignment directory.
+// Note: You'll need to use http-server to display the graphic since you're pulling data in from a source outside of your app.js file.
+
+
+
+
 
 // Students:
 // =========
